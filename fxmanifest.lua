1fa23716ee0f83c87e93b1f2707f829ef68c2942
Kypo <100861025+KyroWasHere@users.noreply.github.com>
2022-06-30 13:30:11 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'adamant'
+game "gta5"
+
+name 'JamesDawson Vape'
+description 'all credits to https://sketchfab.com/3d-models/vape-3d-model-f238a0756d364d3c92eec1b6b9732a40 JamesDawson'
+author 'Kypo'
